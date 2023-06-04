0a614103dcffaf61874f7d2584a1ca8e8a62245e
Andy Lee <a.lee.intl@gmail.com>
2023-06-05 00:56:47 +0200

    Add core models

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from .models import OAuthUser
+from .models import OAuthUser, Wallet, WalletUser, Transaction
 
 admin.site.register(OAuthUser)
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['name', 'balance', 'asset_id', 'name', 'category', 'wallet_type']
+    search_fields = ['name']
+
+@admin.register(WalletUser)
+class WalletUserAdmin(admin.ModelAdmin):
+    list_display = ['wallet', 'user', 'role', 'granted_at', 'invited_by']
+    list_filter = ['role']
+    raw_id_fields = ['wallet', 'user', 'invited_by']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['wallet', 'amount', 'date', 'type', 'added_by']
+    list_filter = ['type']
+    search_fields = ['wallet__name', 'description']
+    raw_id_fields = ['wallet', 'added_by']
--- core/models.py
+++ core/models.py
@@ -1,5 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import decimal
 
 class OAuthUser(AbstractUser):
     pass
+
+class AssetCategory(models.TextChoices):
+    CASH = 'cash'
+    EQUITY = 'equity'
+    CRYPTO = 'crypto'
+    FUND = 'fund'
+    OTHER = 'other'
+
+WALLET_TYPE_CHOICES = [
+    ('PUBLIC', 'Public'),
+    ('PRIVATE', 'Private'),
+]
+
+class Wallet(models.Model):
+    asset_id = models.CharField(max_length=24)
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=10, choices=AssetCategory.choices)
+    balance = models.DecimalField(max_digits=19, decimal_places=8, default=decimal.Decimal('0.00'))
+    wallet_type = models.CharField(max_length=7, choices=WALLET_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+ROLES = [
+    (1, 'Viewer'),
+    (2, 'Editor'),
+    (3, 'Manager'),
+    (4, 'Admin'),
+]
+
+class WalletUser(models.Model):
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='wallet_users')
+    user = models.ForeignKey(OAuthUser, on_delete=models.CASCADE)
+    role = models.IntegerField(choices=ROLES)
+    granted_at = models.DateTimeField(auto_now_add=True)
+    invited_by = models.ForeignKey(OAuthUser, on_delete=models.SET_NULL, null=True, related_name='invitations')
+
+class Transaction(models.Model):
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.DecimalField(max_digits=19, decimal_places=8)
+    date = models.DateTimeField(auto_now_add=True)
+    TRANSACTION_TYPES = [
+        ('INCOME', 'Income'),
+        ('EXPENSE', 'Expense'),
+    ]
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    description = models.CharField(max_length=200, blank=True)
+    added_by = models.ForeignKey(OAuthUser, on_delete=models.CASCADE)
