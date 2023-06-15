b5eeeb439bd90755b5ecd702afc4ddb921aa29b1
Andy Lee <a.lee.intl@gmail.com>
2023-06-15 02:24:18 +0200

    Djoser urls

--- keibo/urls.py
+++ keibo/urls.py
@@ -20,9 +20,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('djoser.urls')),
+    path("api/", include("djoser.urls.jwt")),
     path("core/", include("core.urls")),
-    path('accounts/', include('allauth.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATICFILES_DIRS)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
