3db90fddf1b9cefb3bb3c16ff9552dcfa49fa8eb
Andy Lee <a.lee.intl@gmail.com>
2023-06-16 02:08:57 +0200

    Add user model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,54 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin
+)
 import decimal
+
+
+class KeiboUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Email address is required')
+        email = self.normalize_email(email)
+        email = email.lower()
+
+        user = self.model(email=email, **kwargs)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class KeiboUser(AbstractBaseUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255,unique=True)
+
+    is_active = models.BooleanField(default=False) # Djoser needs it false either way.
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = KeiboUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
 
 class AssetCategory(models.TextChoices):
     CASH = 'cash'
