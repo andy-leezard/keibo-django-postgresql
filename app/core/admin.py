09f156356a707c166e32b6cfe5f47086dea4f9ce
Andy Lee <a.lee.intl@gmail.com>
2023-06-23 01:46:59 +0200

    Display users on admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
-from .models import Wallet, WalletUser, Transaction
+from .models import KeiboUser, Wallet, WalletUser, Transaction
+
+admin.site.register(KeiboUser)
+
 
 @admin.register(Wallet)
 class WalletAdmin(admin.ModelAdmin):
     list_display = ['name', 'balance', 'asset_id', 'name', 'category', 'wallet_type']
     search_fields = ['name']
 
+
 @admin.register(WalletUser)
 class WalletUserAdmin(admin.ModelAdmin):
-    list_display = ['wallet', 'role', 'granted_at'] # 'user' , 'invited_by'
+    list_display = ['wallet', 'role', 'granted_at']  # 'user' , 'invited_by'
     list_filter = ['role']
-    raw_id_fields = ['wallet'] # 'user', 'invited_by'
+    raw_id_fields = ['wallet']  # 'user', 'invited_by'
+
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ['wallet', 'amount', 'date', 'type' ] # 'added_by'
+    list_display = ['wallet', 'amount', 'date', 'type']  # 'added_by'
     list_filter = ['type']
     search_fields = ['wallet__name', 'description']
-    raw_id_fields = ['wallet'] # 'added_by'
+    raw_id_fields = ['wallet']  # 'added_by'
