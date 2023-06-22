a942662a5dfef3fee47ef375673899aaaf08ad13
Andy Lee <a.lee.intl@gmail.com>
2023-06-23 01:47:58 +0200

    Exclude facebook from oauth

--- app/keibo/settings.py
+++ app/keibo/settings.py
@@ -230,8 +230,8 @@ SITE_NAME = 'Keibo'
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
+    # 'social_core.backends.github.GithubOAuth2',
 ]
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = getenv('GOOGLE_AUTH_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = getenv('GOOGLE_AUTH_SECRET_KEY')
