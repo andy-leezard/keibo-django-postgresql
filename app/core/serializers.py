69d707f93666666b696005113c2c7fa23bf8cad4
Andy Lee <a.lee.intl@gmail.com>
2023-08-30 01:22:03 +0200

    Type-transform serializers

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from .models import KeiboUser, WalletUser, Wallet
+from .models import KeiboUser, WalletUser, Wallet, Transaction
 
 
 class WalletSerializer(serializers.ModelSerializer):
+    balance = serializers.SerializerMethodField()
+
     class Meta:
         model = Wallet
         fields = [
@@ -16,11 +18,19 @@ class WalletSerializer(serializers.ModelSerializer):
             'is_public',
         ]
 
+    def get_balance(self, obj):
+        return float(obj.balance)
+
 
 class WalletUserSerializer(serializers.ModelSerializer):
+    granted_at = serializers.SerializerMethodField()
+
     class Meta:
         model = WalletUser
         fields = ['id', 'wallet', 'role', 'granted_at']
+
+    def get_granted_at(self, obj):
+        return int(obj.date.timestamp() * 1000)
 
 
 class KeiboUserSerializer(serializers.ModelSerializer):
@@ -36,3 +46,38 @@ class KeiboUserSerializer(serializers.ModelSerializer):
             'is_superuser',
             'objects',
         ]
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+    gross_amount = serializers.SerializerMethodField()
+    net_amount = serializers.SerializerMethodField()
+    transaction_fee = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Transaction
+        fields = [
+            'id',
+            'category',
+            'recipient',
+            'sender',
+            'confirmed_by_recipient',
+            'confirmed_by_sender',
+            'gross_amount',
+            'net_amount',
+            'transaction_fee',
+            'description',
+            'date',
+        ]
+
+    def get_date(self, obj):
+        return int(obj.date.timestamp() * 1000)
+
+    def get_gross_amount(self, obj):
+        return float(obj.gross_amount)
+
+    def get_net_amount(self, obj):
+        return float(obj.net_amount)
+
+    def get_transaction_fee(self, obj):
+        return float(obj.transaction_fee)
