550d16f3851007f67be9905fe63e6c17a4d0a6bf
Andy Lee <a.lee.intl@gmail.com>
2023-09-01 01:04:07 +0200

    Append new wallet balance on patch transaction

--- app/core/transaction_api.py
+++ app/core/transaction_api.py
@@ -60,10 +60,51 @@ class TransactionCreateView(generics.ListCreateAPIView):
                 except ObjectDoesNotExist:
                     pass
 
+
 class TransactionUpdateView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
 
+        new_balance = None
 
+        # Before saving, check if confirmed_by_recipient or confirmed_by_sender is updated
+        if 'confirmed_by_recipient' in request.data and instance.recipient:
+            if request.data['confirmed_by_recipient']:
+                instance.recipient.balance -= instance.net_amount
+                instance.recipient.save()
+                new_balance = instance.recipient.balance
+            else:
+                instance.recipient.balance += instance.net_amount
+                instance.recipient.save()
+                new_balance = instance.sender.balance
+
+        if 'confirmed_by_sender' in request.data and instance.sender:
+            if request.data['confirmed_by_sender']:
+                instance.sender.balance += instance.net_amount
+                instance.sender.save()
+                new_balance = instance.sender.balance
+            else:
+                instance.sender.balance -= instance.net_amount
+                instance.sender.save()
+                new_balance = instance.sender.balance
+
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        # Append 'new_balance' to the response data
+        data = dict(serializer.data)
+        if new_balance is not None:
+            data['new_balance'] = float(new_balance)
+
+        return Response(data)
