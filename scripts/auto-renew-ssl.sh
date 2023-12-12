667d13563a50d463676887d9bd42c0b2db8e780e
Andy Lee <a.lee.intl@gmail.com>
2023-12-12 02:40:02 +0100

    define aws ec2 auto renew ssl script

--- scripts/auto-renew-ssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd /home/ec2-user/keibo-django-postgresql
-/usr/local/bin/docker-compose -f docker-compose.deploy.yml run --rm certbot certbot renew
---
+++ scripts/aws-ec2-auto-renew-ssl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+# this script is tailored to be used in an AWS ec2 instance as a crontab task on a root level.
+# edit, copy and paste file into an upper level folder from this repo's folder.
+cd /home/ec2-user/keibo-django-postgresql
+/usr/local/bin/docker-compose -f docker-compose.deploy.yml run --rm certbot certbot renew
